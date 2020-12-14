984974dc8f674cd07640eafee93e806d4b19d2bc
crashdemons <crashenator@gmail.com>
2020-12-14 14:29:46 -0600

    other 512-bit hashes supported by hascat / ipv4 check

---
+++ ipv4-address/BLAKE2b-512.bat
@@ -0,0 +1,2 @@
+cd ..\..
+hashcat -m 600 -a 3 Cicada-DWH-HashcatAttempts\deepwebhash-blake2 Cicada-DWH-HashcatAttempts\ipv4-address\ipv4pattern.hcmask -O > Cicada-DWH-HashcatAttempts\ipv4-address\BLAKE2b-512.txt
