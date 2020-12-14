4f7b69958d3b24146a5b36ae299a2cee8d565ee2
crashdemons <crashenator@gmail.com>
2020-12-14 12:53:12 -0600

    sha512 ipv4 address format

---
+++ ipv4-address/sha512.bat
@@ -0,0 +1,2 @@
+cd ..\..
+hashcat -m 1700 -a 3 Cicada-DWH-HashcatAttempts\deepwebhash Cicada-DWH-HashcatAttempts\ipv4-address\ipv4pattern.hcmask -O > Cicada-DWH-HashcatAttempts\ipv4-address\sha512.txt
